c1b13771b8e7fcec0b0b197d62c0924ea80d6d72
Philip Peffer <115198033+PhilipPeffer@users.noreply.github.com>
2026-06-03 14:23:58 +0000

    added VBD calculations

--- calc_sfb16_projections.R
+++ calc_sfb16_projections.R
@@ -3,6 +3,9 @@ library(nflreadr)
 
 season <- readRDS("data/sfb16_season_totals.rds")
 
+# calculate projected sfb16 points
+# 1. make position-specific linear model of ppr pts to sfb16 pts from 2025 data
+# 2. use the models to make predictions on FantasyPros ppr projections
 calc_proj_pts <- function(stats, pos) {
   proj <- read_csv(paste0("data/FantasyPros_Fantasy_Football_Projections_", pos, ".csv"), skip_empty_rows=TRUE)
   proj <- proj |> slice(c(-1, -n()))
@@ -21,24 +24,62 @@ dfs = list()
 for(pos in positions) {
   dfs[[pos]] = calc_proj_pts(season, pos)
 }
-sfb16_proj <- bind_rows(dfs)
+sfb16_proj <- bind_rows(dfs) |> arrange(desc(sfb16_fpts_proj))
+
+## Calculate Value Over Last Starter (VOLS) and Value Over Replacement Player (VORP)
+sfb16_proj <- sfb16_proj |>
+  mutate(QB = if_else(Pos == "QB", TRUE, FALSE)) |>
+  group_by(QB) |>
+  mutate(vols_rank = min_rank(desc(sfb16_fpts_proj))) |>
+  ungroup()
+
+# Last starting QB (assuming 2 per team)
+QB_LS <- sfb16_proj |> filter(QB == TRUE, vols_rank == 2*12)
+print("Last QB Starter:")
+print(QB_LS)
+
+# Last starting non-QB (assuming 8 per team)
+Flex_LS <- sfb16_proj |> filter(QB == FALSE, vols_rank == 8*12)
+print("Last Non-QB Starter:")
+print(Flex_LS)
+
+# # Last bench QB player (assuming 1 per team)
+# QB_LB <- sfb16_proj |> filter(QB == TRUE, vbd_rank == 3*12)
+# print("Last QB on bench:")
+# print(QB_LB)
+# 
+# # Last bench non-QB player (assuming 9 per team)
+# Flex_LB <- sfb16_proj |> filter(QB == FALSE, vbd_rank == 17*12)
+# print("Last non-QB on bench:")
+# print(Flex_LB)
+
+# Last bench player
+LB <- sfb16_proj |> arrange(desc(sfb16_fpts_proj)) |> slice(20*12)
+print("Last player on bench:")
+print(LB)
+
+# sfb16_proj <- sfb16_proj |>
+#   mutate(VOLS = if_else(QB == TRUE,
+#                         sfb16_fpts_proj - QB_LS$sfb16_fpts_proj,
+#                         sfb16_fpts_proj - Flex_LS$sfb16_fpts_proj),
+#          VORP = if_else(QB == TRUE,
+#                         sfb16_fpts_proj - QB_LB$sfb16_fpts_proj,
+#                         sfb16_fpts_proj - Flex_LB$sfb16_fpts_proj),
+#          VBD = VOLS + VORP,
+#          vbd_rank = min_rank(desc(VBD))
+#   ) |>
+#   arrange(vbd_rank)
+
+sfb16_proj <- sfb16_proj |>
+  mutate(VOLS = if_else(QB == TRUE,
+                        sfb16_fpts_proj - QB_LS$sfb16_fpts_proj,
+                        sfb16_fpts_proj - Flex_LS$sfb16_fpts_proj),
+         VORP = sfb16_fpts_proj - LB$sfb16_fpts_proj,
+         VBD = VOLS + VORP,
+         vbd_rank = min_rank(desc(VBD))
+  ) |>
+  arrange(vbd_rank) |>
+  select(-vols_rank)
 
 saveRDS(sfb16_proj, "data/sfb16_projections.rds")
 write_csv(sfb16_proj, "data/sfb16_projections.csv")
-
-# proj_ranks <- try({ # prevents cran errors
-#   load_ff_rankings("draft")
-# })
-# proj_ranks <- proj_ranks |>
-#   filter(page_type == "redraft-overall")
-#
-# m <- lm(total_sfb16_rank ~ total_ppr_rank, data=(season |> filter(position == "QB")))
-# r <- summary(m)$adj.r.squared
-# 
-# qb_proj_ranks <- proj_ranks |>
-#   filter(pos == "QB") |>
-#   arrange(ecr) |>
-#   select(c(player, ecr))
-# y <- predict(m, newdata=(qb_proj_ranks |> select(ecr) |> rename(total_ppr_rank = ecr)))
-# bind_cols(qb_proj_ranks, sfb16_pred=y)
-#rmse <- sqrt(mean(m$residuals^2))
